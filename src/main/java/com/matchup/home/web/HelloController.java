8bb783e127ba68856aa7de78642fc3d9b5169591
minyun02 <www5981@naver.com>
2021-07-08 22:29:51 +0900

    0708 - test 작성 및 gradle version updated, import 수정

--- src/main/java/com/matchup/home/web/HelloController.java
+++ src/main/java/com/matchup/home/web/HelloController.java
@@ -1,4 +1,13 @@
 package com.matchup.home.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
